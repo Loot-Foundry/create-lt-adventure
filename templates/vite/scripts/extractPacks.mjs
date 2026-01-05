98325d055efc7580ca4abdd3d72f0436e80308f0
mrvauxs <mrvauxs@gmail.com>
2026-01-05 15:36:34 +0100

    fancy pants extractPacks

--- templates/vite/scripts/extractPacks.mjs
+++ templates/vite/scripts/extractPacks.mjs
@@ -3,42 +3,45 @@
 import { existsSync } from "node:fs";
 import fs from "node:fs/promises";
 import path from "node:path";
+import * as p from "@clack/prompts";
 import { extractPack } from "@foundryvtt/foundryvtt-cli";
 // import moduleJSON from "../module.json" with { type: "json" };
 
 const foundryDataDir = "packs/";
 const jsonDataDir = "data/";
 
+p.intro(`Extracting ${foundryDataDir} into ${jsonDataDir}...`)
+
 const outDir = path.resolve(process.cwd());
 const packsCompiled = path.resolve(outDir, foundryDataDir);
 if (!existsSync(packsCompiled)) {
-	console.error("Packs directory does not exist in the build!");
+	p.log.warn("Packs directory does not exist in the build!")
 }
 
 const packFolders = await fs.readdir(packsCompiled);
 
-console.log("Cleaning Data Folder");
+const prog = p.progress({ max: packFolders.length })
+
+prog.start("Extracting...")
 
 for (const pack of packFolders) {
 	if (!existsSync(`${jsonDataDir}/${pack}`)) {
 		await fs.mkdir(`${jsonDataDir}/${pack}`);
 	}
-	const files = await fs.readdir(`${jsonDataDir}/${pack}`, { withFileTypes: true });
-	const jsonFiles = files
-		.filter(f => f.isFile() && f.name.toLowerCase().endsWith(".json"))
-		.map(f => f.name);
-	for (const file of jsonFiles) {
-		await fs.rm(path.resolve(jsonDataDir, pack, file));
-	}
-}
-
-for (const pack of packFolders) {
-	console.log(`Extracting pack: ${pack}`);
 	await extractPack(
 		path.resolve(packsCompiled, pack),
 		`${jsonDataDir}/${pack}`,
-		{ },
+		{
+			expandAdventures: true,
+			omitVolatile: true,
+			folders: false,
+			clean: true,
+			log: false
+		},
 	);
+	prog.advance(1, `Extracted /${pack} directory.`)
 }
 
-console.log("Extraction Complete");
+prog.stop("Extraction Complete.")
+
+p.outro("Finished!");
