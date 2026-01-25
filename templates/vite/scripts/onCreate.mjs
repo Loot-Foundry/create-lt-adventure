62344d9b60d308508a3f4cb6c0977367d13df80f
mrvauxs <mrvauxs@gmail.com>
2026-01-25 15:58:34 +0100

    Rename gitignores

--- templates/vite/scripts/onCreate.mjs
+++ templates/vite/scripts/onCreate.mjs
@@ -1,3 +1,5 @@
+import { rename } from "fs";
+
 const mod = (await Bun.file("../module.json").json());
 const pack = (await Bun.file("../package.json").json());
 
@@ -18,5 +20,8 @@ mod.media = [
 
 await Bun.write("../module.json", JSON.stringify(mod, null, "\t"));
 await Bun.write("../package.json", JSON.stringify(pack, null, "\t"));
+
+// Rename gitignore to .gitignore
+rename("../gitignore", "../.gitignore");
 
 export { };
