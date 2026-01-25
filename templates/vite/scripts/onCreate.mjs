14035d9ab30c992087074c3502e12aa6ce234e42
mrvauxs <mrvauxs@gmail.com>
2026-01-25 15:53:51 +0100

    git and media

--- templates/vite/scripts/onCreate.mjs
+++ templates/vite/scripts/onCreate.mjs
@@ -8,6 +8,14 @@ mod.styles = [`dist/${mod.id}.css`];
 // Package
 pack.name = mod.id;
 
+mod.media = [
+	{
+		"auto": true,
+		"type": "setup",
+		"url": `modules/${mod.id}/assets/setup.webp`
+	}
+];
+
 await Bun.write("../module.json", JSON.stringify(mod, null, "\t"));
 await Bun.write("../package.json", JSON.stringify(pack, null, "\t"));
 
