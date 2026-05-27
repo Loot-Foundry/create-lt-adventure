a603d21b187f61ef375d2c40596cb0beb7380f56
mrvauxs <mrvauxs@gmail.com>
2026-05-27 13:33:24 +0200

    add some template css

--- templates/vite/scripts/onCreate.mjs
+++ templates/vite/scripts/onCreate.mjs
@@ -2,14 +2,17 @@ import { renameSync } from "fs";
 
 const mod = (await Bun.file("../module.json").json());
 const pack = (await Bun.file("../package.json").json());
+const css = (await Bun.file("../src/module.css").text());
 
-// Module
-mod.esmodules = [`dist/${mod.id}.js`];
-mod.styles = [`dist/${mod.id}.css`];
+// CSS
+css.replaceAll("module-template", mod.id);
 
 // Package
 pack.name = mod.id;
 
+// Module
+mod.esmodules = [`dist/${mod.id}.js`];
+mod.styles = [`dist/${mod.id}.css`];
 mod.media = [
 	{
 		"type": "setup",
@@ -17,6 +20,8 @@ mod.media = [
 		"thumbnail": `modules/${mod.id}/assets/setup.webp`
 	}
 ];
+
+// Final writes and global renames
 
 const modString = JSON.stringify(mod, null, "\t")
 	// .replaceAll("AUTHOR", data.author)
@@ -28,6 +33,7 @@ const packString = JSON.stringify(pack, null, "\t")
 
 await Bun.write("../module.json", modString);
 await Bun.write("../package.json", packString);
+await Bun.write("../src/module.css", css);
 
 // Rename gitignore to .gitignore
 renameSync("../gitignore", "../.gitignore");
