f95e7991112ba72a63e3bedad1955517e2bf76df
mrvauxs <mrvauxs@gmail.com>
2026-05-27 14:09:56 +0200

    dumb typo

--- addons/github-workflow/setup.mjs
+++ addons/github-workflow/setup.mjs
@@ -13,20 +13,20 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const data = await p.group(
-	{
-		features: () =>
-			p.multiselect({
-				message: "Additional features?",
-				initialValues: [],
-				required: false,
-				options: [
-					// { label: "Prereleases", value: "prereleases" },
-					{ label: "Uploading via FTP", value: "ftp" },
-					{ label: "Discord webhook on updates", value: "discord" },
-				],
-			}),
-	},
-	{ onCancel: () => process.exit(0) },
+  {
+    features: () =>
+      p.multiselect({
+        message: "Additional features?",
+        initialValues: [],
+        required: false,
+        options: [
+          // { label: "Prereleases", value: "prereleases" },
+          { label: "Uploading via FTP", value: "ftp" },
+          { label: "Discord webhook on updates", value: "discord" },
+        ],
+      }),
+  },
+  { onCancel: () => process.exit(0) },
 );
 
 // Grab main.yml template
@@ -39,27 +39,27 @@ let mainYml = mainYmlTemplate;
 
 // I should probably just make this a JSON and convert it to accursed YAML at the end step...
 if (data.features.includes("discord") || data.features.includes("ftp")) {
-	mainYml += `
+  mainYml += `
             # https://stackoverflow.com/questions/61919141/read-json-file-in-github-actions
             - id: set_var
               run: echo "PACKAGE_JSON=$(jq -c . < module.json)" >> $GITHUB_OUTPUT
 
             - name: Get FTP Path
               id: ftp
-              run: echo "ftp=\${{fromJson(steps.set_var.outputs.PACKAGE_JSON).flags.ftpPath}}" >> "$GITHUB_OUTPUT
+              run: echo "ftp=\${{fromJson(steps.set_var.outputs.PACKAGE_JSON).flags.ftpPath}}" >> $GITHUB_OUTPUT
 
             - name: Get Module ID
               id: module_id
-              run: echo "module_id=\${{fromJson(steps.set_var.outputs.PACKAGE_JSON).id}}" >> "$GITHUB_OUTPUT
+              run: echo "module_id=\${{fromJson(steps.set_var.outputs.PACKAGE_JSON).id}}" >> $GITHUB_OUTPUT
 
             - name: Get Module Title
               id: title
-              run: echo "title=\${{fromJson(steps.set_var.outputs.PACKAGE_JSON).title}}" >> "$GITHUB_OUTPUT
+              run: echo "title=\${{fromJson(steps.set_var.outputs.PACKAGE_JSON).title}}" >> $GITHUB_OUTPUT
 `;
 }
 
 if (data.features.includes("ftp")) {
-	mainYml += `
+  mainYml += `
             - name: Put Files into FTP Folder
               env:
                 FTP_PASSWORD: \${{ secrets.FTP_PASSWORD }}
@@ -87,7 +87,7 @@ if (data.features.includes("ftp")) {
 }
 
 if (data.features.includes("discord")) {
-	mainYml += `
+  mainYml += `
             - name: Send Discord Ping
               uses: Ilshidur/action-discord@0.3.2
               env:
@@ -107,9 +107,9 @@ let note = "✅ Installed!";
 note += "\nThe Github workflow is triggered by making a new release. To make a new release go to your repository's Releases page which can be found in the sidebar on the right and press \"Draft a new release.\" Fill in the version number and you're done!"
 
 if (data.features.includes("discord"))
-	note +=
-		"\n - For the Discord integration, make sure to create a DISCORD_WEBHOOK secret with the webhook url.";
+  note +=
+    "\n - For the Discord integration, make sure to create a DISCORD_WEBHOOK secret with the webhook url.";
 if (data.features.includes("ftp"))
-	note += `\n - For the FTP integration, make sure to include the FTP_SERVER, FTP_USERNAME, and FTP_PASSWORD secrets.\n\tThe module JSON also can include a flag stating its subdirectory on the FTP server under ${cyan("flags.ftpPath")}.`;
+  note += `\n - For the FTP integration, make sure to include the FTP_SERVER, FTP_USERNAME, and FTP_PASSWORD secrets.\n\tThe module JSON also can include a flag stating its subdirectory on the FTP server under ${cyan("flags.ftpPath")}.`;
 
 p.note(note, "Github Workflow");
